5c3ab1b8b1bcfd176dad9a6f2be862dcd34a3485
yancymin <senseslofter@gmail.com>
2019-06-06 17:00:58 +0800

    basic card done

---
+++ src/components/card.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="card" :style="'background-color:' + cardColor">
+      <div class="tag">{{tag}}</div>
+    <div class="card_wrap">
+      <div class="des">
+        <h1>{{h1}}</h1>
+        <span>{{des}}</span>
+        <defaultButton buttonText="查看详情"/>
+      </div>
+      <div class="img-area">
+        <img src="../assets/index/geetestweb.png" alt>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import defaultButton from "../components/defaultButton.vue";
+import ghostButton from "../components/ghostButton.vue";
+
+export default {
+  name: "card",
+  props: ["h1", "des", "cardColor", "tag"],
+  components: {
+    defaultButton,
+    ghostButton
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/global.scss";
+
+.card {
+    position: relative;
+  width: 100%;
+  height: 420px;
+  margin: 10px 0;
+  border-radius: 8px;
+  background-color: $white;
+  overflow: hidden;
+  @include flex-all-center;
+
+.tag {
+    z-index: 100;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    padding: 8px 16px;
+    background-color: #D9DBDF;
+    border-radius:50px;
+    @include font(12px,$font-color-black-2, 400)
+    }
+
+  &_wrap {
+    position: relative;
+    width: 100%;
+    @include flex-all-center {
+      justify-content: space-between;
+    }
+    .des {
+        z-index: 1;
+        position: absolute;
+        left: 120px;
+      h1 {
+        word-break: keep-all;
+        margin-bottom: 10px;
+        @include font(40px, $font-color-black-1, 600){
+            line-height: 56px;
+        };
+      }
+      span{
+          display: inline-block;
+          min-width: 600px;
+          margin-bottom: 50px;
+          @include font(14px, $font-color-black-2, 400) {
+              line-height: 20px;
+          }
+      }
+    }
+  }
+
+  .img-area {
+      z-index: 0;
+      position: absolute;
+      right: 0;
+    img {
+      height: 420px;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,9 +24,15 @@
         </div>
       </div>
       <div class="head_container">
-        <div class="logo"></div>
-        <div class="motion"></div>
+        <img src="../assets/index/logo_banner.svg" alt class="logo">
+        <div class="motion">
+          <img src="../assets/index/name_logo.svg" alt class="motion_logo">
+        </div>
       </div>
+    </div>
+    <div class="container">
+      <card h1="我是个设计师" tag="网页设计" des="今天天气真好阿"/>
+      <card h1="我是个设计师" tag="网页设计" des="2018 年网站改版，配合极验的品牌形象，采用了全新的视觉风格。"/>
     </div>
   </div>
 </template>
@@ -34,6 +40,7 @@
 <script>
 import defaultButton from "../components/defaultButton.vue";
 import ghostButton from "../components/ghostButton.vue";
+import card from "../components/card.vue";
 import link from "../assets/index/button-icon/link_icon.svg";
 
 export default {
@@ -41,7 +48,8 @@ export default {
   props: ["buttonIcon"],
   components: {
     defaultButton,
-    ghostButton
+    ghostButton,
+    card
   },
   data() {
     return {
@@ -62,17 +70,49 @@ export default {
 .home {
   width: 100%;
   background-color: $gray;
-  @include flex-all-center {
-    align-items: flex-start;
-  }
+    @include flex-column;
+
 
   .head {
     width: 100%;
     height: 680px;
     background-color: $black;
-
+    overflow: hidden;
+    @include flex-column;
     @include font(14px, $font-color-white-3, 500);
+
+    .nav-1 {
+      width: 90%;
+      padding-top: 24px;
+      @include flex-all-center {
+        justify-content: space-between;
+      }
+
+      > ul {
+        margin: 0 auto;
+        li {
+          display: inline;
+          margin: 0 40px;
+        }
+      }
+    }
+
+    &_container {
+      @include flex-column;
+      margin-top: 120px;
+
+      .motion {
+        &_logo {
+          position: relative;
+        margin-top: 60px;
+          }
+      }
+    }
   }
+
+    .container {
+      width: 90%;
+    }
 }
 </style>
 

43ba155df6176882734475a296a4279cc0588a45
yancymin <senseslofter@gmail.com>
2019-08-19 23:44:42 +0800

    info.js

--- src/data/info.js
+++ src/data/info.js
@@ -2,9 +2,9 @@ export default {
   en: {
     about: [
       'About me',
-      `Hello everyone, I am designer YorKun. In fact, my real name is a bit complicated and the input is so troublesome. For the convenience of myself and others, I simply took the current name with a homonym. Everyone called me "york." </br> </br>
-    Because I was interested in painting from an early age, I usually spent the weekend in painting classes, then I began to study art in high school. The university studied at the Hubei Academy of Fine Arts. It may be that there are more "experimental" and "pure art" in the learning environment. I am more eager to get close to the visual expression of life. I hope to use the knowledge I have learned to solve the problem. I am keen on digital products, The display screen has a special liking, so the junior decided to learn the UI design, and then naturally entered the industry after graduation.</br> </br>
-    Whether it is a firm belief in doing "good" things, or looking for "living" in the sea, I hope that I can always love design, Respect and Struggle.`,
+      `Hey peeps. My name is YorKun. A UI designer and an animation enthusiast. You probably find this name unusual which is coming from my original name, you could simply call me "york".<br/><br/>
+       I started to get obsessed with drawing when I was 12, I remember spending a whole weekend in the drawing class at that time. Naturally, I ended up with studying art in high school. From there, I went to Hubei Academy of Fine Arts to continue pursuing my art career.<br/><br/>
+       Nowadays, I had a chance to work as a UI designer. I love solving problems with my design mindset and hopefully get exposed with more challenges in the future. Respect & Struggle.`,
     ],
     contact: {
       title: 'Contact me',
@@ -13,9 +13,9 @@ export default {
   cn: {
     about: [
       '关于我',
-      `大家好，我是设计师 YorKun。事实上我姓名的字有点儿复杂，以至于输入法里要翻很多页才能找到。为了方便自己和他人，索性以首字母和谐音取了现在的这个别名，大家更习惯叫我 「右可」。<br/><br/>
-               因为从小对画画感兴趣，我小学时周末通常是在绘画兴趣班度过的，随后高中开始系统的学习美术，大学于湖北美术学院动画系就读。可能是在学习环境中接触「实验性」和「纯艺」比较多，我更向往贴近生活的视觉表现形式，希望利用所学知识来解决周身的实际问题，再加上自己对带有显示屏的东西情有独钟，热衷数码产品，所以大三开始学习 UI 设计，毕业后也顺其自然进入到了这个行业。<br/><br/>
-               无论是坚持自我做「好」事，还是在大海之中寻「活」法，我希望自己能一直热爱设计，持谦逊之心、尽拼搏之力。`,
+      `你好，我叫 YorKun，是一名设计师，从事视觉、界面、动效等设计工作。你可以简单称呼我「右可」。<br/><br/>
+       因为对画画感兴趣，我小学时周末通常是在绘画兴趣班度过的，随后高中开始系统的学习美术，大学于湖北美术学院动画系就读。可能是在学习环境中接触「实验性」和「纯艺」比较多，我更向往贴近生活的视觉表现形式，希望利用所学知识来解决实际问题。<br/><br/>
+       我希望自己能一直热爱设计并敢于面对更多挑战，持谦逊之心、尽拼搏之力。`,
     ],
     contact: {
       title: '联系我',
--- src/views/gmake.vue
+++ src/views/gmake.vue
@@ -6,7 +6,7 @@
         <img :src="item.src" alt v-for="(item, index) in imgSrc" :key="index" />
       </section>
     </div>
-    <articleInfo :links='links' date='更新于 2019.7.12'/>
+    <articleInfo :links="links" date="更新于 2019.7.12" />
     <globalFooter></globalFooter>
     <top />
   </div>
@@ -14,69 +14,70 @@
 
 
 <script>
-import articleNav from '../components/articleNav.vue';
-import globalFooter from '../components/globalFooter.vue';
-import top from '../components/top.vue';
-import articleInfo from '../components/articleInfo.vue';
+import articleNav from "../components/articleNav.vue";
+import globalFooter from "../components/globalFooter.vue";
+import top from "../components/top.vue";
+import articleInfo from "../components/articleInfo.vue";
 
 export default {
-  name: 'Article-1',
+  name: "Article-1",
   components: {
     articleNav,
     globalFooter,
     top,
-    articleInfo,
+    articleInfo
   },
   data() {
     return {
       links: [
         {
-          title: '「行为验证」在线体验',
-          href: 'https://www.geetest.com/show',
+          title: "「行为验证」在线体验",
+          href: "https://www.geetest.com/show"
         },
         {
-          title: '《滑动拼图验证背后的故事》',
-          href: 'https://geetest.design/#/article/2019-05-23?time=2019-05-23%20%E5%8F%91%E5%B8%83&tags=%E5%8E%9F%E5%88%9B%E5%86%85%E5%AE%B9&tags=UI&view=1023',
-        },
+          title: "《滑动拼图验证背后的故事》",
+          href:
+            "https://geetest.design/#/article/2019-05-23?time=2019-05-23%20%E5%8F%91%E5%B8%83&tags=%E5%8E%9F%E5%88%9B%E5%86%85%E5%AE%B9&tags=UI&view=1023"
+        }
       ],
 
       imgSrc: [
         {
-          src: require('../assets/article/gmake/GM-01.gif'),
+          src: require("../assets/article/gmake/GM-01.gif")
         },
         {
-          src: require('../assets/article/gmake/GM-02.png'),
+          src: require("../assets/article/gmake/GM-02.png")
         },
         {
-          src: require('../assets/article/gmake/GM-03.png'),
+          src: require("../assets/article/gmake/GM-03.png")
         },
         {
-          src: require('../assets/article/gmake/GM-04.png'),
+          src: require("../assets/article/gmake/GM-04.png")
         },
         {
-          src: require('../assets/article/gmake/GM-05.png'),
+          src: require("../assets/article/gmake/GM-05.png")
         },
         {
-          src: require('../assets/article/gmake/GM-06.png'),
+          src: require("../assets/article/gmake/GM-06.png")
         },
         {
-          src: require('../assets/article/gmake/GM-07.png'),
+          src: require("../assets/article/gmake/GM-07.png")
         },
         {
-          src: require('../assets/article/gmake/GM-08.png'),
+          src: require("../assets/article/gmake/GM-08.png")
         },
         {
-          src: require('../assets/article/gmake/GM-09.png'),
+          src: require("../assets/article/gmake/GM-09.png")
         },
         {
-          src: require('../assets/article/gmake/GM-10.png'),
+          src: require("../assets/article/gmake/GM-10.png")
         },
         {
-          src: require('../assets/article/gmake/GM-11.png'),
+          src: require("../assets/article/gmake/GM-11.png")
         }
-      ],
+      ]
     };
-  },
+  }
 };
 </script>
 
@@ -87,7 +88,7 @@ export default {
 
 .globalFooter {
   margin-top: 160px;
-      width: calc(100% - 32px);
-    padding: 24px 0 40px 0;
+  width: calc(100% - 32px);
+  padding: 24px 0 40px 0;
 }
 </style>

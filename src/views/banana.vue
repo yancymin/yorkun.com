74278edbfb6e51bfd7f765d4a2d83c121a6cd81e
yancymin <senseslofter@gmail.com>
2019-09-18 14:31:44 +0800

    updated: banana page

--- src/views/banana.vue
+++ src/views/banana.vue
@@ -3,10 +3,12 @@
     <articleNav />
     <div class="article-wrap">
       <section>
-        <img :src="item.src" alt v-for="(item, index) in imgSrc" :key="index" />
+        <span v-for="(item, index) in imgSrc" :key="index">
+          <img :src="item.src" alt />
+        </span>
       </section>
     </div>
-    <articleInfo :links='links' date='更新于 2019.8.27'/>
+    <articleInfo :links="links" date="更新于 2019.8.27" />
     <globalFooter></globalFooter>
     <top />
   </div>
@@ -14,35 +16,44 @@
 
 
 <script>
-import articleNav from '../components/articleNav.vue';
-import globalFooter from '../components/globalFooter.vue';
-import top from '../components/top.vue';
-import articleInfo from '../components/articleInfo.vue';
+import articleNav from "../components/articleNav.vue";
+import globalFooter from "../components/globalFooter.vue";
+import top from "../components/top.vue";
+import articleInfo from "../components/articleInfo.vue";
 
 export default {
-  name: 'banana',
+  name: "banana",
   components: {
     articleNav,
     globalFooter,
     top,
-    articleInfo,
+    articleInfo
   },
   data() {
     return {
       links: [
         {
-          title: '行为验证 在线体验',
-          href: 'https://www.geetest.com/show',
+          title: "行为验证 在线体验",
+          href: "https://www.geetest.com/show"
         }
       ],
 
       imgSrc: [
         {
-          src: require('../assets/article/banana/Banana-01.png'),
+          src: require("../assets/article/banana/Banana-01.png")
+        },
+        {
+          src: require("../assets/article/banana/Banana-02.png")
+        },
+        {
+          src: require("../assets/article/banana/Banana-03.jpg")
+        },
+        {
+          src: require("../assets/article/banana/Banana-04.jpg")
         }
-      ],
+      ]
     };
-  },
+  }
 };
 </script>
 
@@ -51,9 +62,33 @@ export default {
 @import "./src/style/global.scss";
 @import "./src/style/mobile.scss";
 
+section {
+  span {
+    display: inherit;
+    &:nth-of-type(4) {
+      position: relative;
+
+      &::before {
+        position: absolute;
+        left: 15.78%;
+        top: 6.45%;
+        content: "";
+        display: block;
+        width: 68.8%;
+        height: 58.67%;
+        background: url(/img/Banana-04.7efa6728.gif) no-repeat;
+        background-size: cover;
+        transform: rotate(-1deg);
+        border-radius: 4px;
+        box-shadow: 0 5px 13px -6px rgba(93,57,44,0.71), 0 8px 23px 0 rgba(96,58,46,0.24);
+      }
+    }
+  }
+}
+
 .globalFooter {
   margin-top: 160px;
-      width: calc(100% - 32px);
-    padding: 24px 0 40px 0;
+  width: calc(100% - 32px);
+  padding: 24px 0 40px 0;
 }
 </style>
